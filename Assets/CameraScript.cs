ad3670e342e35e05a7fda4d5cd497e98111197a2
Daniel Manganaro <manganaro.daniel@gmail.com>
2018-10-08 17:14:04 +0800

    Project Setup

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public float rotateSpeed = 30.0f;
+    public Color color1 = Color.red;
+    public Color color2 = Color.blue;
+    public float duration = 3.0f;
+
+    Camera m_Cam;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Cam = GetComponent<Camera>();
+        m_Cam.clearFlags = CameraClearFlags.SolidColor;
+
+        RandomiseColours();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the camera
+        transform.Rotate(Vector3.forward, Time.deltaTime * rotateSpeed);
+
+        // Change background colour
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        m_Cam.backgroundColor = Color.Lerp(color1, color2, t);
+    }
+
+    public void RandomiseColours()
+    {
+        color1 = new Color(Random.value, Random.value, Random.value, 1.0f);
+        color2 = new Color(Random.value, Random.value, Random.value, 1.0f);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 600f;
+
+    float m_Movement = 0f;
+    int m_Score = 0;
+    CameraScript m_Cam;
+
+    private void Start()
+    {
+        m_Cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraScript>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        m_Movement = Input.GetAxisRaw("Horizontal");
+
+        if(m_Score % 50 == 0 && m_Score != 0) {
+            m_Cam.RandomiseColours();
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, m_Movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void AddScore(int value)
+    {
+        m_Score = m_Score + value;
+    }
+
+    public int GetScore()
+    {
+        return m_Score;
+    }
+}
